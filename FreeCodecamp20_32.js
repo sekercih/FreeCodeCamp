875d14b101149f43069803a3b683889d4c761650
sekercih <hkkskrc@gmail.com>
2021-09-30 15:35:15 +0300

    freecodecamp workshop

---
+++ FreeCodecamp20_32.js
@@ -0,0 +1,48 @@
+/* /* // Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+myStr[0] = "H"; // Change this line
+
+// Only change code above this line
+//myStr = "Hello Word";
+
+console.log(myStr);
+console.log(myStr[0]);
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var thirdLetterOfLastName = lastName[2]; // Change this line
+console.log(thirdLetterOfLastName);
+
+*/ /*
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
+
+console.log(lastLetterOfLastName);
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length-2]; // Change this line
+
+console.log(secondToLastLetterOfLastName); */
+
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb; // Change this line
+// Only change code above this line
+
+console.log(wordBlanks);
+
